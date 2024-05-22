0dc67be9a520799b8a932a61b4b8d94cc3ea584b
Christopher Chiarandini <chiarandini.christopher@gmail.com>
2024-05-22 11:22:19 +0200

    test netlify 3.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
-import { vercelPreset } from "@vercel/remix/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-installGlobals();
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      presets: [vercelPreset],
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
 });
